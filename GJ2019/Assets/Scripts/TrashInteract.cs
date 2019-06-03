d158831a085cc7a82bffe637d0c2f18bc0fe6ad0
IHudson <cuffy94@gmail.com>
2019-06-03 12:18:18 +0100

    Trash interact and score script (TMP added)

---
+++ GJ2019/Assets/Scripts/TrashInteract.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashInteract : MonoBehaviour
+{
+    [SerializeField]
+    bool playerOverTrash = false;
+    [SerializeField]
+    GameObject player = null;
+
+    void Update()
+    {
+        if(playerOverTrash)
+        {
+            //allow for pickup
+            if(Input.GetButtonDown("Fire1"))
+            {
+                //pickup trash
+                //assign trash to player
+                Debug.Log("Trash pickup");
+                player.GetComponent<PlayerScore>().InceremtnScore(1);
+
+                playerOverTrash = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check for player
+        if (other.tag == "Player")
+        {
+            playerOverTrash = true;
+            player = other.gameObject;
+        }
+        Debug.Log("Collision");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerOverTrash = false;
+            player = null;
+        }
+    }
+}
