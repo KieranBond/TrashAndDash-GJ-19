a5cf4b080d3eadf35946cbe774051a10f7f0936b
Chewy <ashleyycheemaa@hotmail.com>
2019-06-03 12:46:46 +0100

    Updated inputs

--- GJ2019/Assets/Scripts/PlayerMovement.cs
+++ GJ2019/Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetJoystickNames().Length > 0)
+        if (Input.GetJoystickNames().Length > 0)
         {
             Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             transform.position += movement * movementSpeed * Time.deltaTime;
-            //transform.rotation
+
+            if (movement != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), Time.deltaTime * 10.0f);
+            }
         }
     }
 }
