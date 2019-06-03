ab40cdb399eeb7e905f321e766732717b6a8e9da
IHudson <cuffy94@gmail.com>
2019-06-04 00:56:35 +0100

    big ship

--- GJ2019/Assets/Scripts/BigShip/BigShipMovement.cs
+++ GJ2019/Assets/Scripts/BigShip/BigShipMovement.cs
@@ -59,7 +59,7 @@ public class BigShipMovement : MonoBehaviour
     {
         bool drop = true;
         int index = 0;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
         while (drop)
         {
             //drop things 
@@ -72,7 +72,7 @@ public class BigShipMovement : MonoBehaviour
             {
                 drop = false;
             }
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.35f);
         }
     }
 
