9ce44133ab0b5599e6033796aad76b7d3e2ec9c7
IHudson <cuffy94@gmail.com>
2019-06-04 09:39:34 +0100

    it just works -Todd Howard

--- GJ2019/Assets/Scripts/MainMenuUI.cs
+++ GJ2019/Assets/Scripts/MainMenuUI.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField]
+    GameObject gameObject;
+    [SerializeField]
     GameObject evnAssets;
+
+    Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = evnAssets.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        evnAssets.transform.position = new Vector3(0,1,0) * Mathf.Sin(Time.time) * 0.5f;
     }
 
     public void PlayGame()
     {
-
+        evnAssets.SetActive(false);
+        gameObject.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     public void QuitGame()
